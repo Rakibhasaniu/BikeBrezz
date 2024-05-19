fe85c1da575772cc33a9c597e9dad0151865f16f
Rakibhasaniu <rakibhasanoyoniu@gmail.com>
2024-05-19 15:24:54 +0600

    project setup done and define login ,register,navbar and dahboard

--- src/app/(withCommonLayout)/layout.tsx
+++ src/app/(withCommonLayout)/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+// import "./globals.css";
 import { Providers } from "@/lib/Provider";
+import NavBar from "./components/shared/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,10 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <Providers>{children}</Providers>
-      </body>
-    </html>
+    <div >
+        <NavBar></NavBar>
+        <div className="mx-auto container">{children}</div>
+      <p>footer</p>
+    </div>
   );
 }
